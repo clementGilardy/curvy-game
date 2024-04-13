829500e5d372d53fd4fbd1251f9a2e97a8dba16e
Gilardy clément <gilardy.clement@gmail.com>
2024-04-13 12:41:24 +0200

    feat: first init

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+mod splash;
+mod game;
+mod menu;
+
+use bevy::{
+    prelude::*,
+};
+use bevy::input::keyboard::KeyboardInput;
+use splash::splash::splash_plugin;
+use menu::menu::menu_plugin;
+use game::game::game_plugin;
+#[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
+pub enum GameState {
+    #[default]
+    Splash,
+    Menu,
+    Game,
+}
+
+// One of the two settings that can be set through the menu. It will be a resource in the app
+#[derive(Resource, Debug, Component, PartialEq, Eq, Clone, Copy)]
+enum DisplayQuality {
+    Low,
+    Medium,
+    High,
+}
+
+// One of the two settings that can be set through the menu. It will be a resource in the app
+#[derive(Resource, Debug, Component, PartialEq, Eq, Clone, Copy)]
+struct Volume(u32);
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(DisplayQuality::Medium)
+        .insert_resource(Volume(7))
+        .init_state::<GameState>()
+        .add_systems(Startup, setup)
+        .add_plugins((splash_plugin, menu_plugin, game_plugin))
+        .run();
+}
+
+fn setup(mut commands: Commands) -> () {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn despawn_screen<T: Component>(to_despawn: Query<Entity, With<T>>, mut commands: Commands) {
+    for entity in &to_despawn {
+        commands.entity(entity).despawn_recursive();
+    }
+}
